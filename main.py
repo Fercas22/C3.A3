edc152e088582170bff2ff7fba262c36a6d9314d
Hola <fernando.fmet@gmail.com>
2022-04-04 13:31:56 -0500

    Primer commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from pickletools import optimize
+from tabnanny import verbose
+import zipfile
+import os
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from keras_preprocessing import image
+from keras_preprocessing.image import ImageDataGenerator
+import numpy as np
+
+# datos_entrenamiento = datos['Vegetables/train']
